81a9b6e5fde8bf7f1c76a5990bad643d2dabe617
Roberto Meza <romeza@mx1.ibm.com>
2014-12-22 22:17:27 -0600

    New functionality has added. The method load, is now working. All method was renamed to be more Pythonic. Added support to direct dictionary access. Added support to DataBase, it works with myquerybuilder. The method list, is now working.

--- pydbproperties.py
+++ pydbproperties.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python2
 
+from __future__ import print_function
 import re
+import sys
+
+try:
+    import myquerybuilder
+except:
+    print('MySql Simple Query Builder module not found')
+    print('pip install MysqlSimpleQueryBuilder')
+    sys.exit(1)
+    pass
+
+NULL = ('', None)
 
 
 class pydbproperties():
     """
     A Python implements of pyjavaproperties for database
     """
-    def __init__(self, database=None, table_name=None):
+    def __init__(self):
         # Dictionary of properties.
         self._props = {}
-        self._database = database
-        self._table_name = table_name
         self.bspacere = re.compile(r'\\(?!\s$)')
+        self.othercharre = re.compile(r'(?<!\\)(\s*\=)|(?<!\\)(\s*\:)')
+
         # Dictionary mapping keys from property
         # Dictionary to pristine dictionary
         self._keymap = {}
@@ -20,25 +32,29 @@ class pydbproperties():
         # using the 'store' method
         self._origprops = {}
         self._keyorder = []
+        # Connection to DB
+        self._conn = None
+        # Table name for properties
+        self._table_name = 'pydbproperties'
         pass
 
-    def getProperty(self, key):
+    def get_property(self, key):
         """ Return a property for the given key """
 
         return self._props.get(key, '')
 
-    def setProperty(self, key, value):
+    def set_property(self, key, value):
         """ Set the property for the given key """
         if type(key) is str and type(value) is str:
-            if len(key) != 0 and len(value) != 0:
-                self.processPair(key, value)
+            if len(key) != 0:
+                self.process_pair(key, value)
             else:
-                raise ValueError, "'both key and value can't be null!"
+                raise ValueError("key can't be null!")
         else:
-            raise TypeError, 'both key and value should be strings!'
+            raise TypeError('both key and value should be strings!')
         pass
 
-    def processPair(self, key, value):
+    def process_pair(self, key, value):
         """ Process a (key, value) pair """
 
         oldkey = key
@@ -88,36 +104,199 @@ class pydbproperties():
             self._keyorder.append(key)
         pass
 
-    def list(self):
+    def list(self, out=sys.stdout):
         """ Prints a listing of the properties to the
         stream 'out' which defaults to the standard output """
 
+        if out == sys.stdout or type(out) is file:
+            out.write('-- listing properties --\n')
+            for key, value in self._props.items():
+                out.write(''.join((key, '=', value, '\n')))
+                pass
+            pass
+        else:
+            raise TypeError('Argument should be a file or sys.stdout object!')
         pass
 
-    def getPropertyDict(self):
+    def get_property_dict(self):
         return self._props
 
-    def store(self, database=None, table_name=None):
-        if database is None and self._database is None:
-            raise ValueError, "Database can't be None"
-        if table_name is None and self._table_name is None:
-            raise ValueError, "TableName can't be None"
+    def store(self):
+        raise NotImplementedError
+
+    def load(self):
+        if self._conn is None:
+            raise ValueError('Connection not initialized')
+        attr = ('key', 'value')
+        if self._table_name in NULL:
+            raise ValueError('Table name can\'t be null')
+        properties_dict = self._conn.select(attr, self._table_name)
+        properties_list = [b.get('key') + '=' + b.get('value') +
+                           '\n' for b in properties_dict]
+        self.__parse(properties_list)
         pass
 
-    def load(self, database, table_name):
+    def __parse(self, lines):
+        """ Parse a list of lines and create
+        an internal property dictionary """
+
+        # Every line in the file must consist of either a comment
+        # or a key-value pair. A key-value pair is a line consisting
+        # of a key which is a combination of non-white space characters
+        # The separator character between key-value pairs is a '=',
+        # ':' or a whitespace character not including the newline.
+        # If the '=' or ':' characters are found, in the line, even
+        # keys containing whitespace chars are allowed.
+
+        # A line with only a key according to the rules above is also
+        # fine. In such case, the value is considered as the empty string.
+        # In order to include characters '=' or ':' in a key or value,
+        # they have to be properly escaped using the backslash character.
+
+        # Some examples of valid key-value pairs:
+        #
+        # key     value
+        # key=value
+        # key:value
+        # key     value1,value2,value3
+        # key     value1,value2,value3 \
+        #         value4, value5
+        # key
+        # This key= this value
+        # key = value1 value2 value3
+
+        # Any line that starts with a '#' or '!' is considerered a comment
+        # and skipped. Also any trailing or preceding whitespaces
+        # are removed from the key/value.
+
+        # This is a line parser. It parses the
+        # contents like by line.
+
+        lineno = 0
+        i = iter(lines)
+
+        for line in i:
+            lineno += 1
+            line = line.strip()
+            # Skip null lines
+            if not line:
+                continue
+            # Skip lines which are comments
+            if line[0] in ('#', '!'):
+                continue
+            # Some flags
+            # escaped = False
+            # Position of first separation char
+            sepidx = -1
+            # A flag for performing wspace re check
+            # flag = 0
+            # Check for valid space separation
+            # First obtain the max index to which we
+            # can search.
+            m = self.othercharre.search(line)
+            if m:
+                first, last = m.span()
+                start, end = 0, first
+                # flag = 1
+                wspacere = re.compile(r'(?<![\\\=\:])(\s)')
+            else:
+                if self.othercharre2.search(line):
+                    # Check if either '=' or ':' is present
+                    # in the line. If they are then it means
+                    # they are preceded by a backslash.
+
+                    # This means, we need to modify the
+                    # wspacere a bit, not to look for
+                    # : or = characters.
+                    wspacere = re.compile(r'(?<![\\])(\s)')
+                start, end = 0, len(line)
+
+            m2 = wspacere.search(line, start, end)
+            if m2:
+                # print 'Space match=>',line
+                # Means we need to split by space.
+                first, last = m2.span()
+                sepidx = first
+            elif m:
+                # print 'Other match=>',line
+                # No matching wspace char found, need
+                # to split by either '=' or ':'
+                first, last = m.span()
+                sepidx = last - 1
+                # print line[sepidx]
+                pass
+
+            # If the last character is a backslash
+            # it has to be preceded by a space in which
+            # case the next line is read as part of the
+            # same property
+            while line[-1] == '\\':
+                # Read next line
+                nextline = i.next()
+                nextline = nextline.strip()
+                lineno += 1
+                # This line will become part of the value
+                line = line[:-1] + nextline
+                pass
+
+            # Now split to key,value according to separation char
+            if sepidx != -1:
+                key, value = line[:sepidx], line[sepidx+1:]
+            else:
+                key, value = line, ''
+            self._keyorder.append(key)
+            self.process_pair(key, value)
+            pass
         pass
 
-    def propertyNames(self):
+    def set_table_name(self, table_name):
+        if table_name not in NULL:
+            self._table_name = table_name
+        else:
+            raise ValueError('Table name can\'t be null')
+        pass
+
+    def get_table_name(self):
+        return self._table_name
+
+    def get_property_names(self):
         """ Return an iterator over all the keys of the property
         dictionary, i.e the names of the properties """
 
         return self._props.keys()
+
+    def __getitem__(self, name):
+        """ To support direct dictionary like access """
+
+        return self.get_property(name)
+
+    def __setitem__(self, name, value):
+        """ To support direct dictionary like access """
+
+        self.set_property(name, value)
+        pass
+
+    def conn(self, **kwargs):
+        self._conn = myquerybuilder.QueryBuilder(**kwargs)
+        pass
     pass
 
 
 if __name__ == "__main__":
     a = pydbproperties()
-    for b in range(10):
-        a.setProperty('key' + str(b), 'value' + str(b))
-    print a.getPropertyDict()
-    print a.getProperty('key11')
+    #for b in range(10):
+    #    a.set_property('key' + str(b), 'value' + str(b))
+    #    pass
+    #print(a.get_property_dict())
+    #print(a.get_property('key11'))
+    # a.store()
+    config = {
+        "host": 'localhost',
+        "user": 'root',
+        "passwd": '',
+        "db": 'test_pydbproperties',
+    }
+
+    a.conn(**config)
+    a.load()
+    print(a.get_property_dict())
